5c9bbf4b5069d8c52b75f12feffb603fda20f9ce
murilotrigo_ <murilo.trigo@auditornfe.com.br>
2023-03-28 11:50:31 -0300

    sub-menu-anotacoes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const menuLateral = document.querySelector('.menu-lateral')
 const aulas = document.querySelector('.aulas')
+const anotacoes = document.querySelector('.anotacoes')
 const fechaMenu = document.querySelector('.btnFecha')
 const abreMenu  = document.querySelector('.btnAbre')
 
@@ -17,5 +18,12 @@ aulas.addEventListener('click',() => {
     const itensAulas = document.querySelector('.itens-aulas')
     const seta = document.querySelector('.fa-caret-right')
     itensAulas.classList.toggle('mostra')
-    seta.classList.toggle('seta')
+    seta.classList.toggle('seta1')
+})
+
+anotacoes.addEventListener('click',() => {
+    const itensAnotacoes = document.querySelector('.itens-anotacoes')
+    const seta2 = document.querySelector('.fa-caret-right seta2')
+    itensAnotacoes.classList.toggle('mostra')
+    seta2.classList.toggle('seta2')
 })
